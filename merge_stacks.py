8a92d659ddd0cf11aafc46dde3292190aa803c0f
felix cc <felix.meyenhofer@unifr.ch>
2017-05-03 20:46:29 +0200

    Rework the merge_stacks.py
    - allow different file name mappings (file, time, sequential or none)
    - add the input for the file extension (was hardcoded .stk, now can be
      arbitrairly choosen)
    - allow for more than two channels

--- merge_stacks.py
+++ merge_stacks.py
@@ -1,80 +1,126 @@
-# @File(label='Choose the directory containing the .stk-files', style='directory') inputDir
-# @String(label='File name pattern channel 1', value='*GFP*stk') pattern_ch1
-# @String(label='File name pattern channel 2', value='*mCherry*stk') pattern_ch2
+# @File(label='Input directory', style='directory') input_dir
+# @String(label='Input file extension', value='.stk') extension
+# @File(label='Output directory', style='directory') output_dir
+# @String (label='Channel patterns separated by semicolon', value='mCherry;GFP') patterns
+# @String(label='File naming (mapping)', value='None', choices={'None', 'Sequential', 's -> y', 's -> x'}) mapping
+# @Boolean (label='Arrange output images in time-sub-directories', value=True) subdirs
+
+# @LogService log
 
 
-import os
-from time import sleep
+import os, re
 from glob import glob
-from ij import IJ, WindowManager
-from ij.io import DirectoryChooser
+from ij import IJ, WindowManager, ImagePlus, ImageStack, CompositeImage
+from ij.io import FileSaver
 
 
 def run():
-    print 'Running time-point stitching'
-    print 'Working directory: %s' % (inputDir)
+    target_dir = output_dir.getPath()
+    
+    log.info('Running merge_stacks.py')
+    log.info('\tInput directory: %s' % input_dir)
+    log.info('\tOutput directory %s' % output_dir)
 
     # Get the files
-    ch1 = glob(os.path.join(inputDir.getPath(), pattern_ch1))
-    ch2 = glob(os.path.join(inputDir.getPath(), pattern_ch2))
-    nch1 = len(ch1)
-    nch2 = len(ch2)
+    ch = {}
+    for pattern in patterns.split(";"):
+        ch[pattern] = glob(os.path.join(input_dir.getPath(), "*" + pattern + "*" + extension))
+        ch[pattern].sort()  # Sort the file names to make sure from the channels come in the same order.
 
-    if nch1 % 2 == 1:
-        print 'The number of files for channel 1 (%i) should be pair (as in pairwise stitching).' % nch1
+    keys = ch.keys()
+    channels = len(keys)
+
+    # Sanity checks
+    if channels < 2:
+        log.error('\tFound only one channel, nothing to merge. Put at least two channel patterns separated by semicolon.')
         return
 
-    if (nch2 % 2) == 1:
-        print 'The number of files for channel 2 (%i) should be pair(as in pairwise stitching).' % nch2
-        return
+    n = 0
+    for p in range(channels - 1):
+        n = len(ch[keys[p]])
+        if n != len(ch[keys[p + 1]]):
+            log.error('The number of file for both channels have to be the same. found %i and %i.' % (nch1, nch2))
+            return
 
-    if nch1 != nch2:
-        print 'The number of file for both channels have to be the same. found %i and %i.' % (nch1, nch2)
-        return
+    log.info('\tMerging %i image sets:' % n)
+    for i in range(n):
 
-    # Sort the file names to make sure the corresponding files from the channels come in the
-    # desired order.
-    ch1.sort()
-    ch2.sort()
+        msg = '\t'
+        fn = {}
+        # Separate path and file name
+        for pattern in keys:
+            fn[pattern] = os.path.basename(ch[pattern][i])
+            msg += fn[pattern] + ', '
 
-    print 'Merging:'
-    for item1, item2 in zip(ch1, ch2):
-        # Extract file name information and create an output file name with x-y grid coordinates
-        fn1 = os.path.basename(item1)
-        fn2 = os.path.basename(item2)
-        experiment, _, field, time = fn2[:-4].split('_')
-        merge_name = '%s_x1_y%s_t%02d' % (experiment, field[1:], int(time[1:]))
+        log.info(msg[:-2])
 
-        # Create an output directory per time-point
-        time_dir_name = 't%02d' % (int(time[1:]))
-        output_dir = os.path.join(inputDir.getPath(), time_dir_name)
-        if os.path.exists(output_dir):
-            pass
-        #			print '\t\toutput directory %s already exists' % output_dir
-        else:
-            os.mkdir(output_dir)
+        # extract experiment name from the filename
+        experiment = fn[keys[0]].split('_')[0]
+        merge_name = experiment + '_'
 
-        print '\t%s and %s' % (fn1, fn2)
+        # extract fieldnumber from the file name
+        if mapping == 'None':
+            merge_name = fn[keys[0]].replace(extension, '')
+            for pattern in keys:
+                for part in pattern.split('*'):
+                    merge_name = merge_name.replace(part, '')
+        elif mapping == 'Sequential':
+            merge_name += '%03d' % i
+        elif 's ->' in mapping:
+            match = re.search('.*_s(\d{1,2})_.*', fn[keys[0]])
+            if match:
+                if mapping == 's -> y':
+                    merge_name += 'x1_y%s' % match.group(1)
+                elif mapping == 's -> x':
+                    merge_name += 'x%s_y1' % match.group(1)
+            else:
+                log.error('\tCould not extract the field number from the file name. Consider using no mapping (None).')
+                return
 
-        output_file = os.path.join(output_dir, merge_name + '.tif')
+        # extract time-point number from the file name
+        match = re.search('.*_t(\d{1,2})\.[A-Za-z0-9]{3}$', fn[keys[0]])
+        if match and (mapping != 'None'):
+            time = match.group(1)
+            time_dir_name = 't%02d' % (int(time))
+            merge_name += '_t%02d' % int(time)
+            if subdirs:
+                target_dir = os.path.join(output_dir.getPath(), time_dir_name)
+                if not os.path.exists(target_dir):
+                    os.mkdir(target_dir)
+
+        # Merge stacks
+        output_file = os.path.join(target_dir, merge_name + '.tif')
         if os.path.exists(output_file):
-            print '\t\talready processed'
+            log.info('\t\talready processed: %s' % output_file)
         else:
-            IJ.open(os.path.join(inputDir, item1))
-            IJ.open(os.path.join(inputDir, item2))
+            imps = {}
+            for pattern in keys:
+                imps[pattern] = IJ.openImage(ch[pattern][i])
 
-            IJ.run('Merge Channels...', 'c1=' + fn1 + ' c2=' + fn2 + ' create')
-            sleep(1)
-            imp = WindowManager.getImage('Composite')
-            imp.setTitle(merge_name)
-            imp.updateAndRepaintWindow()
+            slices = imps[keys[0]].getNSlices()
+            width = imps[keys[0]].getWidth()
+            height = imps[keys[0]].getHeight()
 
-            IJ.save(imp, output_file)
-            print '\t\tsaving:  %s' % (output_file)
-        IJ.run('Close All')
+            # Create the new image
+            stk = ImageStack(width, height)
+            for slice in range(1, slices + 1):
+                for channel, pattern in enumerate(keys):
+                    channel += 1  # enumeration from 1
+                    imps[pattern].setPosition(channel, slice, 1)
+                    stk.addSlice(None, imps[pattern].getProcessor())
 
-    print ('Done.')
+            imp = ImagePlus(merge_name, stk)
+            imp.setDimensions(channels, slices, 1)
+            comp = CompositeImage(imp, CompositeImage.COLOR)
+            #            comp.show()
+            saver = FileSaver(comp)
+            saver.saveAsTiff(output_file)
+
+            log.info('\t\tsaving:  %s' % (output_file))
+
+    log.info('\tDone.')
 
 
 if __name__ == '__main__':
+    IJ.run("Console", "uiservice=[org.scijava.ui.DefaultUIService [priority = 0.0]]")
     run()
